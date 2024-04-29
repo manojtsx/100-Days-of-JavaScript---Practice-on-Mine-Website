9e7c5c3d9275dc6d2292ccadcc48f5c01cef1f87
Manoj Shrestha <shresthanewar678@gmail.com>
2024-04-29 16:52:11 +0545

    100daysofJS day33

--- src/app/challenges.ts
+++ src/app/challenges.ts
@@ -703,4 +703,26 @@ export const challenges: Challenge[] = [
                 `,
     difficulty: "easy",
   },
+  {
+    id : 33,
+    title : 'Day 33 : Random Hex Color Generator',
+    description : `Coding Challenge : Random Hex Color Generator
+    ===========================================================
+    Write a function randomHexColor that generates a random hexadecimal color code each time it is called. The function should return a string representing the random color code in the format '#RRGGBB', where RR, GG, and BB are two digit hexadecimal numbers representing the red, green, and blue components of the color respectively.
+    ===========================================================
+    Your challenge is to implement the randomHexColor function using JavaScript and ensure that it produces a valid hexadecimal color code with a length of 7 characters ( including the # symbol).
+    ===========================================================
+    CONSTRAINTS:
+    1. The output color code should always with # followed by six hexadecimal characters. (RRGGBB)
+    2. The function should not take any parameters.
+    3. The generated color codes should be unique and evenly distributed across the entire range of possible hexadecimal color coes 
+    ===========================================================
+    HINT: Conver the random number into hexadecimal string reprsentation.
+    console.log(randomHexColor());`,
+    placeholder : `function randomHexColor(){
+        // Your code here
+    }
+    randomHexColor();`,
+    difficulty : 'medium'
+  }
 ];
