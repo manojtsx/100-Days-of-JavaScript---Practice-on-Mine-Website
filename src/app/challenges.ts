fd27b9a675348818cc44bc394e35b9bcd07f56d3
Manoj Shrestha <shresthanewar678@gmail.com>
2024-05-04 19:54:15 +0545

    100daysofJS day38.js

--- src/app/challenges.ts
+++ src/app/challenges.ts
@@ -831,4 +831,23 @@ export const challenges: Challenge[] = [
     `,
     difficulty: "easy",
   },
+  {
+    id : 38,
+    title : 'Day 38 : Number of Days Between Dates',
+    description : `Challenge : Number of Days Between Two Dates
+    ===========================================================
+    Write a function calculateDaysBetweenDates that calculates the number of days between two dates. The date will be provided in YYYY-MM-DD
+    ===========================================================
+    Hint:
+    Days = 24*60*60*1000
+    ===========================================================
+    console.log(calculateDaysBetweenDates("2024-01-01","2024-01-30")) //OUTPUT : 29`,
+    placeholder : `
+    function calculateDaysBetweenDates(date1, date2){
+      // Your code here
+    }
+    calculateDaysBetweenDates("2024-01-01","2024-01-30")  
+      `,
+    difficulty : 'medium'
+  }
 ];
