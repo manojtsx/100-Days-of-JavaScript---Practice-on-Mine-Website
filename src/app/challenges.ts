a8e67d6305c29eb7ab00b46833ff1527f12c1fef
Manoj Shrestha <shresthanewar678@gmail.com>
2024-05-10 16:50:23 +0545

    100daysofJS day43

--- src/app/challenges.ts
+++ src/app/challenges.ts
@@ -947,5 +947,30 @@ export const challenges: Challenge[] = [
     }
     console.log(validateCreditCard("4539 1488 0343 6467));`,
     difficulty : 'medium'
+  },
+  {
+    id : 43,
+    title : 'Challenge 43 : Print FizzBuzz',
+    description : `Challenge : FizzBuzz Challenge
+    ===========================================================
+    Write a function fizzBuzz that accepts two numbers, startNum and endNum, as input. The function should return an array containing numbers and specific strings based on the following rules:
+    ===========================================================
+    //For each number i in the range from startNum to endNum(both inclusive):
+    //If i is divisible by both 3 and 5, include "FizzBuz" in the result.
+    //If i is divisible by only 3, include "Fizz" in the result.
+    //If i is divisible by only 5, include "Buzz" in the result.
+    //Otherwise, include the number i itself.
+    ===========================================================
+    EXAMPLE: 
+    console.log(fizzBuzz(1,15));
+    ===========================================================
+    OUTPUT:
+    [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz", 11, "Fizz",13,14,"FizzBuzz"];`,
+    placeholder : `function fizzBuzz(startNum, endNum){
+      // Your code here
+    }
+    fizzBuzz(1,15);
+    `,
+    difficulty : "medium"
   }
 ];
